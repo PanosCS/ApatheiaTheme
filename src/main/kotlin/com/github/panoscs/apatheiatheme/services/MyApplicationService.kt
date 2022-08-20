551c2a6ca7e7d6cea3fe3de09d5ba1f3fef70ff6
GitHub Action <action@github.com>
2022-08-20 19:40:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/panoscs/apatheiatheme/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/panoscs/apatheiatheme/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.panoscs.apatheiatheme.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.panoscs.apatheiatheme.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/panoscs/apatheiatheme/services/MyProjectService.kt
+++ src/main/kotlin/com/github/panoscs/apatheiatheme/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.panoscs.apatheiatheme.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.panoscs.apatheiatheme.MyBundle
 
 class MyProjectService(project: Project) {
 
